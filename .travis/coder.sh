4dc050830a1e658cb460848329d7564cf53578c9
Jared Whiklo <jwhiklo@gmail.com>
2018-05-04 21:31:58 -0500

    find the directory

--- .travis/coder.sh
+++ .travis/coder.sh
@@ -4,7 +4,8 @@ if [ -n "${TRAVIS_PHP_VERSION}" ]; then
   if [ "${TRAVIS_PHP_VERSION}" == "5.3.3" ]; then
     drush coder-review --reviews=production,security,style,i18n,potx,sniffer islandora_basic_collection
   else
-    $TOOLS_DIR/vendor/bin/phpcs --standard=drupal,drupalpractice --ignore=vendor,*.md,*.info,*.txt sites/all/modules/islandora_basic_collection
+    echo "pwd is $(pwd)"
+    $TOOLS_DIR/vendor/bin/phpcs --standard=drupal,drupalpractice --ignore=vendor,*.md,*.info,*.txt ${TRAVIS_BUILD_DIR}
   fi
 else
     echo "Unable to determine PHP version"
